d675e222ccb393cfdaf9c40fab9bc8e371ed4074
Mihail Dragnev <bymykel5@gmail.com>
2023-01-25 00:24:14 +0100

    chore: add tailwind.

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: colors.zinc,
+      },
+    },
+  },
+  plugins: [],
+};

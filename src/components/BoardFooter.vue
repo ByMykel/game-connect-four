f4b6d9892b0ab860789cb58427cf2e3b380d45ba
Mihail Dragnev <bymykel5@gmail.com>
2023-02-02 00:33:11 +0100

    feat: board footer animation.

--- src/components/BoardFooter.vue
+++ src/components/BoardFooter.vue
@@ -16,12 +16,33 @@ const props = defineProps<{
   <div
     class="h-20 w-[335px] sm:w-[632px] z-20 absolute bottom-[-4rem] sm:-bottom-14 flex justify-center"
   >
-    <div v-if="!gameIsOver" class="relative flex justify-center">
-      <span class="absolute top-14 text-6xl text-white font-bold"
-        >{{ timerCount }}s</span
-      >
-      <TurnBackgroundRed v-if="player === RED_PLAYER" />
-      <TurnBackgroundYellow v-else />
-    </div>
+    <Transition name="footer">
+      <div v-if="!gameIsOver" class="relative flex justify-center">
+        <span class="absolute top-14 text-6xl text-white font-bold"
+          >{{ timerCount }}s</span
+        >
+        <TurnBackgroundRed v-if="player === RED_PLAYER" />
+        <TurnBackgroundYellow v-else />
+      </div>
+    </Transition>
   </div>
 </template>
+
+<style>
+.footer-enter-active {
+  animation: footer-in 0.2s;
+}
+
+.footer-leave-active {
+  animation: footer-in 0.2s reverse;
+}
+
+@keyframes footer-in {
+  0% {
+    transform: translateY(120px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
